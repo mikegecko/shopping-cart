dcd410048ef90087c6aef22fb39c23b630e39751
Michael Guedko <mjgeko@gmail.com>
2022-12-20 11:54:34 -0500

    Add folders for organizing files + remove tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, Router } from 'react-router-dom';
+
+/*---------- App Planning -----------
+
+  Pages:
+    - Homepage
+    - Store page
+    - Cart page
+
+
+
+
+
+
+
+
+
+
+*/
+
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
