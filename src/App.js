2dc938b6038c3ee913da058412ddd898751be8af
Michael Guedko <mjgeko@gmail.com>
2022-12-20 13:04:49 -0500

    Basic pages with navigation
        - Add BrowserRouter, Navbar
        - Errorpage can probably be deleted later

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import './App.css';
-import { BrowserRouter, Route, Routes, Router } from 'react-router-dom';
-
+import "./App.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import Product from "./routes/products";
+import Navbar from "./components/Navbar";
 /*---------- App Planning -----------
 
   Pages:
     - Homepage
     - Store page
-    - Cart page
-
-
-
-
-
-
-
-
-
+    - Cart / Checkout page ( Cart should be a modal )
+    - Contact page
 
 */
-
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
