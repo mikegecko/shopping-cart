3c1240b3fb1c7dd4faa6dfcb054d5773b2b86324
Michael Guedko <mjgeko@gmail.com>
2022-12-20 13:04:16 -0500

    Add routes for different pages

---
+++ src/routes/root.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+export default function Root() {
+  return (
+    <div>
+      <div>
+        <h1>Root Page</h1>
+        <p>Welcome to the Root page!</p>
+      </div>
+    </div>
+  );
+}
