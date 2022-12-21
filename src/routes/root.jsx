243f5f4c30dc2f5afb72e6783a6dde86018d6bfc
Michael Guedko <mjgeko@gmail.com>
2022-12-21 11:30:24 -0500

    Add featured section to Home with some debug style

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,22 @@
 import { Routes, Route, Link } from 'react-router-dom';
-
+import bg from '../images/room1.jpeg'
+import chair from '../images/modern-chair.jpeg';
 export default function Root() {
   return (
-    <div className='home'>
-      <div>
-        <h1>Root Page</h1>
-        <p>Welcome to the Root page!</p>
+    <div className='home-container'>
+      <div className="overlay">
+          <div className='home'>
+            <h1><i>Fake Furniture</i></h1>
+            <h2>Transform your space, elevate your style.</h2>
+            <Link className="link" to="/products">Shop now</Link>
+            <div className='featured'>
+                <div className='out'>1</div>
+                <div className='mid'>2</div>
+                <div className='center'>3</div>
+                <div className='mid'>4</div>
+                <div className='out'>5</div>
+            </div>
+          </div>
       </div>
     </div>
   );
