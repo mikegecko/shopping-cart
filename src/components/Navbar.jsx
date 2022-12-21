e73f83960620fe8a562f863a87c9a5b853d97b6d
Michael Guedko <mjgeko@gmail.com>
2022-12-21 11:29:34 -0500

    Add icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
     return(
@@ -8,6 +10,14 @@ export default function Navbar() {
                 <Link className="link" to="/products">Products</Link>
                 <Link className="link" to="/contact">Contact</Link>
             </nav>
+            <div className="iconbar">
+                <button className="search Ibtn">
+                    <SearchIcon />
+                </button>
+                <button className="cart Ibtn">
+                    <ShoppingCartOutlinedIcon />
+                </button>
+            </div>
         </div>
     )
 }
