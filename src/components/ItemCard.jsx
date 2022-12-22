3a2c2b60c6047de09e13bb46ce6e8159d4de9952
Michael Guedko <mjgeko@gmail.com>
2022-12-21 19:52:13 -0500

    Add ItemCard component boilerplate

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,12 @@
+
+
+export default function ItemCard(){
+    
+    return(
+        <div>
+            {/* Name | Picture | Price | Stock | ID */}
+            {/* Add to cart, quantity adding to card */}
+            
+        </div>
+    )
+}
