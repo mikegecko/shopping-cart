021c02f127269edb5b6f33462e23d49073869a75
Michael Guedko <mjgeko@gmail.com>
2022-12-26 17:06:25 -0500

    Add ItemCard component stucture

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,12 +1,26 @@
 
 
-export default function ItemCard(){
-    
+export default function ItemCard(props){
+    const stock = () => {
+        if(props.product.qty > 0){
+            return(
+                <p className="in-stock">In Stock</p>
+            )
+        }
+        else{
+            return(
+                <p className="out-stock">Out of Stock</p>
+            )
+        }
+    }
     return(
-        <div>
+        <div className="card">
             {/* Name | Picture | Price | Stock | ID */}
             {/* Add to cart, quantity adding to card */}
-            
+            <h4>{props.product.name}</h4>
+            <img src={props.product.img} alt="product" />
+            <p>${props.product.price}</p>
+            <p>{stock()}</p>
         </div>
     )
 }
